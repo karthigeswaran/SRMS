b3c703db1f872fc9040a6a751931833e73ca754a
Karthigeswaran Arumugam <karthigeswaran1995@gmail.com>
2020-12-02 11:46:50 +0530

    Second commit: added student as class.

--- src/com/karthi/handler/StudentRecordHandler.java
+++ src/com/karthi/handler/StudentRecordHandler.java
@@ -6,7 +6,7 @@ import java.sql.*;
 
 public class StudentRecordHandler {
     static final String JDBC_driver  = "com.mysql.jdbc.Driver";
-    static final String DB_URL = "jdbc:mysql://localhost/schooldb";
+    static final String DB_URL = "jdbc:mysql://localhost/srms";
 
     //  Database credentials
     static final String USER = "root";
@@ -14,6 +14,16 @@ public class StudentRecordHandler {
 
     private Connection connection = null;
     private Statement statement = null;
+    private String command = "";
+
+    private void openConnection(){
+        try{
+            connection = DriverManager.getConnection(DB_URL,USER,PASS);
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public StudentRecordHandler(){
         openConnection();
     }
@@ -26,17 +36,22 @@ public class StudentRecordHandler {
             connection.setAutoCommit(false);
             Savepoint savepoint = connection.setSavepoint();
             statement = connection.createStatement();
+            command = "INSERT INTO student "+
+                    "VALUE ("+
+                    student.getName()+","+
+                    student.getClassName()+","+
+                    student.getSection()+","+
+                    Date.valueOf(student.getDob())+","+
+                    student.getRoll_no()+");";
+            int rowCount = statement.executeUpdate(command);
+            if(rowCount==0){
+                return false;
+            }
         }catch(Exception e){
             e.printStackTrace();
+            return false;
         }
         return true;
-    }
-    private void openConnection(){
-        try{
-            connection = DriverManager.getConnection(DB_URL,USER,PASS);
-        }catch(Exception e) {
-            e.printStackTrace();
-        }
     }
 
 }
--- src/com/karthi/servlet/StudentServlet.java
+++ src/com/karthi/servlet/StudentServlet.java
@@ -1,6 +1,7 @@
 package com.karthi.servlet;
 
 import com.google.gson.Gson;
+import com.karthi.handler.StudentRecordHandler;
 import com.karthi.model.Student;
 
 import javax.servlet.http.HttpServlet;
@@ -10,12 +11,18 @@ import java.io.IOException;
 
 public class StudentServlet extends HttpServlet{
     public void doPost(HttpServletRequest request,HttpServletResponse response) throws IOException {
-         String str = request.getReader().readLine();
-         Gson gson = new Gson();
-         Student student = gson.fromJson(str,Student.class);
+        String str = request.getReader().readLine();
+        Gson gson = new Gson();
+        Student student = gson.fromJson(str,Student.class);
         response.getOutputStream().print("successful");
-        //HttpSession session = request.getSession();
-
+        StudentRecordHandler studentRecordHandler = new StudentRecordHandler();
+        if(studentRecordHandler.createStudent(student)){
+            response.setStatus(200);
+            response.getOutputStream().print("New student record successfully created");
+        }else{
+            response.setStatus(400);
+            response.getOutputStream().print("Record not created");
+        }
 
     }
 }
